051664e2ae6c4148b2d7233d16585bb30729e186
Byron Rodriguez <byronrdz@gmail.com>
2015-01-24 14:28:00 -0500

    Code for matrix inversion using cache
    makeCacheMatrix creates a matrix like an object with properties that can
    mantain his status. The variable 'x' is replaced by 'mtrx' because is a
    more descriptive name.
    
    cacheSolve obtains the inverse of the matrix, but if the inverse has
    already been calculated simply uses the cache. 'x' has been replaced by
    'mt' because is a more descriptive name.

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,32 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(mtrx = matrix()) {
+  mtrx_inv <- NULL
+  set <- function(y) {
+    mtrx <<- y
+    mtrx_inv <<- NULL
+  }
+  get <- function() mtrx
+  setinverse <- function(m_i) mtrx_inv <<- m_i
+  getinverse <- function() mtrx_inv
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
 
 ## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(mt, ...) {
+        ## Return a matrix that is the inverse of 'mt'
+  mt_inv <- mt$getinverse()
+  if(!is.null(mt_inv)) {
+    message("getting cached inverse matrix")
+    return(mt_inv)
+  }
+  matriz <- mt$get()
+  mt_inv <- solve(matriz, ...)
+  mt$setinverse(mt_inv)
+  mt_inv
 }
